7accc494593a687236c63cadc55cd81fd5a8d82f
muhdinmussema <muhdinmussema@gmail.com>
2024-02-24 00:34:41 +0300

    Changed url from local to azure domain

--- UserManagement/usermanagement.client/src/App.jsx
+++ UserManagement/usermanagement.client/src/App.jsx
@@ -17,37 +17,37 @@ import UserList from './pages/users/UserList';
 import EditUser from './pages/users/EditUser';
 import AddUser from './pages/users/AddUser';
 import UsersRole from './pages/usersrole/UsersRole';
-export const Login_BASE_URL = "https://localhost:44314";
-export const Register_BASE_URL = "https://localhost:44314";
-export const Role_BASE_URL = "https://localhost:44314";
-export const BASE_URL = "https://localhost:44314";
+export const Login_BASE_URL = "https://usermsapi.azurewebsites.net";
+export const Register_BASE_URL = "https://usermsapi.azurewebsites.net";
+export const Role_BASE_URL = "https://usermsapi.azurewebsites.net";
+export const BASE_URL = "https://usermsapi.azurewebsites.net";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
+    return (
+        <div className="App">
+            <Routes>
 
-        <Route path='/' Component={Login} />
-        <Route path='/register' Component={Register} />
+                <Route path='/' Component={Login} />
+                <Route path='/register' Component={Register} />
 
 
-        {/*Private Route */}
-        <Route element={<PrivateRoutes />}>
-        
-          <Route path='/dashboard' Component={Dashboard} />
-          <Route path='/dashboard/add-role' Component={AddRole} />
-          <Route path='/dashboard/roles' Component={RoleList} />
-          <Route path='/dashboard/edit-role/:id' Component={EditRole} />
-          <Route path='/dashboard/add-user' Component={AddUser} />
-          <Route path='/dashboard/users' Component={UserList} />
-          <Route path='/dashboard/usersrole' Component={UsersRole} />
+                {/*Private Route */}
+                <Route element={<PrivateRoutes />}>
 
-          <Route path='/dashboard/edit-user/:id' Component={EditUser} />
-        </Route>
-      </Routes>
-      <Footer />
-    </div>
-  );
+                    <Route path='/dashboard' Component={Dashboard} />
+                    <Route path='/dashboard/add-role' Component={AddRole} />
+                    <Route path='/dashboard/roles' Component={RoleList} />
+                    <Route path='/dashboard/edit-role/:id' Component={EditRole} />
+                    <Route path='/dashboard/add-user' Component={AddUser} />
+                    <Route path='/dashboard/users' Component={UserList} />
+                    <Route path='/dashboard/usersrole' Component={UsersRole} />
+
+                    <Route path='/dashboard/edit-user/:id' Component={EditUser} />
+                </Route>
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
